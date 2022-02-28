2e70858bc57572b8cbf359d334b51ce34a123bb4
Dan <danilobomfim.si@gmail.com>
2022-02-28 18:53:40 -0300

    Done

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+    API_URL: "http://localhost:5000",
+    
+    MONGO_CONNECTION:'mongodb+srv://cluster0.ahjjg.mongodb.net/myFirstDatabase?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority'
+}
